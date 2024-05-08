198de6deb1471e994cd2e730b84c03648681df74
kusemererwaedson <kusemererwaedson2000@gmail.com>
2024-05-08 18:31:09 +0300

    eson branch

--- core/urls.py
+++ core/urls.py
@@ -10,12 +10,8 @@ urlpatterns = [
     path('login/', views.login_view, name = 'login'),
     path('logout/', views.logout_view, name = 'logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-<<<<<<< HEAD
-    path('feedback/', views.feedback, name='feedback'),
-=======
 
     path('staff/', views.staff, name='staff'),
->>>>>>> b4c5539f17716ed228a1969231b4c2ee2f0ea5f1
    
     path('staff/', views.staff, name='staff'),
 
